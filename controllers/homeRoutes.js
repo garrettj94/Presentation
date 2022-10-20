29483c7a24e9c5b64423ee1ac3888b840c74573f
Brian McKenna <brianmckennaman@gmail.com>
2022-10-19 21:32:59 -0400

    update again

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,20 +10,25 @@ router.get('/', async (req, res) => {
 
         res.render('homepage', {
             departments,
-            logged_in: req.session.logged_in,
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// router.get('/login', (req, res) => {
-//     if (req.session.logged_in) {
-//         res.redirect('/');
-//         return
-//     }
+router.get('/login', (req, res) => {
+    // if (req.session.logged_in) {
+    //     res.redirect('/');
+    //     return
+    // }
 
-//     res.render('login')
-// });
+    res.render('login')
+});
+
+router.get('/createdepartment', (req, res) => {
+    res.render('new')
+})
+
+router.get('/')
 
 module.exports = router;
