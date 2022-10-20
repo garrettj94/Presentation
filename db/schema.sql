1f352d29c3e8c9a24c4fbfd78638888cbac4d420
Brian McKenna <brianmckennaman@gmail.com>
2022-10-19 20:38:44 -0400

    update

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS team_db;
+CREATE DATABASE team_db;
+
+USE team_db;
+
+-- CREATE TABLE department (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   department_name VARCHAR(30) NOT NULL
+-- );
+
+
+-- CREATE TABLE manager (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   manager_name VARCHAR(30) NOT NULL,
+--   department_id INT,
+--   total_enrolled INT
+-- )
+
+-- CREATE TABLE employee (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   employee_name VARCHAR(30) NOT NULL,
+--   department_id INT,
+--   manager_id INT,
+--   total_enrolled INT
+-- )
+
+
